92e6261f4adfa5fc6b36f1602621d3512f102faa
Min <wyes91622@gmail.com>
2023-08-10 02:15:36 +0800

    feat: combined with redis process

--- js/summary.js
+++ js/summary.js
@@ -1,8 +1,35 @@
-import { summary_api_url } from "./config.js";
+import { summary_api_url, api_url } from "./config.js";
 
-export async function getSummary(payload) {
-    const vm = window.vm
-    const converter = OpenCC.Converter({ from: 'cn', to: 'tw' });
+export async function getSummary() {
+    const key = await getRedisKey();
+    fetchSummary(key);
+}
+
+async function getRedisKey() {
+    const topic = "戰爭";
+    const start = "2022-01-03";
+    const end = "2022-01-31";
+    const dateRange = 30;
+    const isExactMatch = false;
+    const searchMode = 0;
+    const response = await fetch(
+        `${api_url}Redis/${topic}/StatrDate/${start}/EndDate/${end}?DateRange=${dateRange}&IsExactMatch=${isExactMatch}&SearchMode=${searchMode}`,
+        {
+            method: "GET",
+            cache: "no-cache",
+            keepalive: true,
+            headers: {
+                "Content-Type": "application/json",
+                "ngrok-skip-browser-warning": "true",
+            },
+        }
+    );
+    return response.text();
+}
+
+async function fetchSummary(key) {
+    const vm = window.vm;
+    const converter = OpenCC.Converter({ from: "cn", to: "tw" });
 
     const response = await fetch(`${summary_api_url}/summary`, {
         method: "POST",
@@ -12,7 +39,7 @@ export async function getSummary(payload) {
             "Content-Type": "application/json",
             Accept: "text/event-stream",
         },
-        body: JSON.stringify(payload),
+        body: JSON.stringify({ content: key }),
     });
 
     const reader = response.body.getReader();
