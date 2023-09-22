11ebd5617470ddd92fc50c2fff78fba6a72b41da
dannylin901022 <dannylin901022@gmail.com>
2023-09-22 18:52:12 +0800

    version 2023/9/22

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 export const summary_api_url = "https://a5ce-35-202-124-27.ngrok.io";
 //export const api_url = "https://localhost:56364/api/";
-export const api_url = "https://9a54-2001-c011-b807-585c-b01d-e7d4-8878-8972.ngrok-free.app/api/";
+export const api_url = "https://acf7-2001-b011-b807-585c-71b1-d9e1-6e13-ac3c.ngrok-free.app/api/";
